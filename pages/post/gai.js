4d3b92636c272946a499e9a071749e972347d953
din <dina.vinter@sap.com>
2021-05-18 05:56:08 +0300

    'gai'

--- pages/post/gai.js
+++ pages/post/gai.js
@@ -2,7 +2,7 @@ import React from "react";
 import Post from "../Post";
 
 export default function GAI() {
-    return <Post  title={'Consent'} video={'Txx2XcJyMY8'} >
+    return <Post  title={'Consent'} video={'VWMUd4ouTTA'} >
 
         <span className={"center align-center"}   ><h1 className="minor align-center">No downtime please! </h1></span>
         <span><h3 className="center align-center minor"> Something is better then nothing!(</h3></span>
